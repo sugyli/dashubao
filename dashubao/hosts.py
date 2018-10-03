cd7c2af69e00410f32c9af47c889fc218846f515
“liunxsugyli” <3101967255@qq.com>
2018-10-03 13:42:07 +0800

    1.009-1

--- dashubao/hosts.py
+++ dashubao/hosts.py
@@ -4,4 +4,6 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'm', 'dashubao.mobileurls', name='m'),
+    host(r'ww', settings.ROOT_URLCONF, name='ww'),
+    host(r'mm', 'dashubao.mobileurls', name='mm'),
 )
