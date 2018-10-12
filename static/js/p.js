a685e6ad71a73ed8276c8d1413c068c934edcba1
“liunxsugyli” <3101967255@qq.com>
2018-10-12 12:03:00 +0800

    1.016

--- static/js/p.js
+++ static/js/p.js
@@ -137,3 +137,27 @@ $(document).ready(function(){
 	});
 
 });
+
+function other(){
+  //JS链接推送
+    (function(){
+		var bp = document.createElement('script');
+		var curProtocol = window.location.protocol.split(':')[0];
+		if (curProtocol === 'https') {
+			bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+		}
+		else {
+			bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+		}
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(bp, s);
+	})();
+}
+
+function webtj(){
+    other();
+
+}
+function waptj(){
+    other();
+}
